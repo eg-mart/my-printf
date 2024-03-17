d68ae78f10eb5a648b0a6871c547e7583196ff03
eg-mart <marteg71@gmail.com>
2024-03-18 01:32:24 +0300

    implement all printf flags in my_printf

--- printf/main.c
+++ printf/main.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
+#include <limits.h>
 
-void print_num_hex(char *buf, unsigned long num);
-void print_num_dec(char *buf, unsigned long num, int is_signed);
-void print_num_bin(char *buf, unsigned long num);
-void print_string(char *buf, char *str);
-void print_char(char *buf, char str);
+int my_printf(const char *fmt, ...);
 
 int main() {
     char buf[500] = "";
 
-    unsigned long n = 0;
-    scanf("%lu", &n);
-
-    print_num_dec(buf, n, 0);
-    print_num_hex(buf + 30, n);
-    print_num_bin(buf + 60, n);
-    print_string(buf + 300, "Hello world, I am Egor!");
-    print_char(buf + 400, '?');
-    
-    puts(buf);
-    puts(buf + 30);
-    puts(buf + 60);
-    puts(buf + 300);
-    puts(buf + 400);
+    int res = my_printf("%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b\n",
+                        -1, -1, "love", 3802, 100, 33, 126,
+                        -1, "love", 3802, 100, 33, 126);
     
     return 0;
 }
--- printf/my_printf.s
+++ printf/my_printf.s
@@ -1,3 +1,24 @@
+section .rodata
+HexDigits   db "0123456789ABCDEF"
+
+align 8
+MyPrintfJmpTable:
+    dq my_printf.case_error
+    dq my_printf.case_bin
+    dq my_printf.case_char
+    dq my_printf.case_dec
+    times 'o' - 'd' - 1 dq my_printf.case_error
+    dq my_printf.case_oct
+    times 's' - 'o' - 1 dq my_printf.case_error
+    dq my_printf.case_str
+    times 'u' - 's' - 1 dq my_printf.case_error
+    dq my_printf.case_unsigned
+    times 'x' - 'u' - 1 dq my_printf.case_error
+    dq my_printf.case_hex
+
+section .bss
+buf         resb 2048
+
 section .text
 extern my_printf
 extern print_num_dec
@@ -6,34 +27,144 @@ extern print_num_bin
 extern print_string
 extern print_char
 
+my_printf:
+    pop r11
+
+    push r9
+    push r8
+    push rcx
+    push rdx
+    push rsi
+
+    push r11
+    push rbp
+    mov rbp, rsp
+    push rbx
+
+    mov rsi, rdi
+    mov rdi, buf
+    mov rbx, 2
+
+.process_symbol:
+    lodsb
+
+    cmp al, 0
+    je .print_buf
+
+    cmp al, '%'
+    je .process_variable
+
+    stosb
+    
+    jmp .process_symbol
+
+.print_buf:
+    mov rax, 1
+    mov rdx, rdi
+    sub rdx, buf
+    mov rdi, 1
+    mov rsi, buf
+    syscall
+
+.exit:
+    pop rbx
+    pop rbp
+    pop r11
+    add rsp, 5 * 8
+    push r11
+
+    ret
+
+.process_variable:
+    xor rax, rax
+    lodsb
+
+    cmp al, '%'
+    je .case_percent
+
+    cmp al, 'a'
+    jb .case_error
+    cmp al, 'x'
+    ja .case_error
+    sub rax, 'a'
+
+    push rsi
+    mov rsi, [rbp + 8 * rbx]
+    inc rbx
+
+    jmp [MyPrintfJmpTable + 8 * rax]
+
+.case_percent:
+    mov byte [rdi], '%'
+    inc rdi
+    jmp .process_symbol
+
+.case_bin:
+    call print_num_bin
+    jmp .common_end
+
+.case_dec:
+    mov rdx, 1
+    call print_num_dec
+    jmp .common_end
+
+.case_unsigned:
+    xor rdx, rdx
+    call print_num_dec
+    jmp .common_end
+
+.case_hex:
+    call print_num_hex
+    jmp .common_end
+
+.case_oct:
+    call print_num_oct
+    jmp .common_end
+
+.case_str:
+    call print_string
+    jmp .common_end
+
+.case_char:
+    call print_char
+    jmp .common_end
+
+.case_error:
+    mov rax, -1
+    jmp .exit
+
+.common_end: ; some cases have the same end, so here it is. name might to change
+    pop rsi
+    jmp .process_symbol
+
 ;--------------------------------------------------------------
 print_num_dec:
-    mov r11, 10000000000000000000
+    mov r11, 1000000000
     mov rcx, 10
 
     cmp rdx, 0
     je .find_first_digit
 
-    mov rax, rsi
-    shr rax, 63
-    cmp rax, 0
+    mov eax, esi
+    shr eax, 31
+    cmp eax, 0
     je .find_first_digit
 
     mov rax, '-'
     stosb
 
     xor rax, rax
-    dec rax
-    sub rax, rsi
-    inc rax
-    mov rsi, rax
+    dec eax
+    sub eax, esi
+    inc eax
+    mov esi, eax
 
 .find_first_digit:
     xor rdx, rdx
-    mov rax, rsi
+    mov eax, esi
     div r11
     
-    cmp rax, 0
+    cmp eax, 0
     jne .print_digit
 
     xor rdx, rdx
@@ -46,7 +177,7 @@ print_num_dec:
 
 .print_digit:
     xor rdx, rdx
-    mov rax, rsi
+    mov eax, esi
     div r11
     xor rdx, rdx
     div rcx
@@ -98,6 +229,37 @@ print_num_hex:
 ;--------------------------------------------------------------
 
 ;--------------------------------------------------------------
+print_num_oct:
+    mov rcx, 63
+
+.find_first_digit:
+    mov rax, rsi
+    shr rax, cl
+    and rax, 0x7
+
+    cmp rax, 0
+    jne .print_digit
+
+    sub cl, 3
+    cmp cl, 0
+    jg .find_first_digit
+
+.print_digit:
+    mov rax, rsi
+    shr rax, cl
+    and rax, 0x7
+
+    add rax, 0x30
+    stosb
+
+    sub cl, 3
+    cmp cl, 0
+    jge .print_digit
+
+    ret
+;--------------------------------------------------------------
+
+;--------------------------------------------------------------
 print_num_bin:
     mov rcx, 63
 
@@ -133,18 +295,19 @@ print_string:
 
 .print_char:
     lodsb
-    stosb
     cmp al, 0
-    jne .print_char
+    je .exit
+    stosb
+    jmp .print_char
 
+.exit:
     ret
 ;--------------------------------------------------------------
 
 ;--------------------------------------------------------------
 print_char:
-    mov [rdi], rsi
+    mov rax, rsi
+    mov byte [rdi], al
+    inc rdi
     ret
 ;--------------------------------------------------------------
-
-section .rodata
-HexDigits   db "0123456789ABCDEF"
